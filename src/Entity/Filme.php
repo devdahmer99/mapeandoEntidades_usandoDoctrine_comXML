ba55925227c5e4b9849b5273634ba6bcb42e0771
Eduardo Dahmer <eduardodahmer99@gmail.com>
2022-02-12 11:56:30 -0300

    criando a entidade de filmes e mapemando a mesma de acordo com o banco de dados

---
+++ src/Entity/Filme.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Filmes\Doctrine\Entity;
+
+class Filme
+{
+    private $id;
+    private $titulo;
+    private $sinopse;
+    private $anoLancamento;
+    private $ultimaAtualizacao;
+}
